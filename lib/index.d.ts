84cb6dc0d825a39c573064991a91507497beeccf
fuadop <fuadolatunji@gmail.com>
2021-07-16 13:24:48 +0100

    fix: uses module export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,1 @@
-import { SendchampConstructor } from './constants/interfaces';
-import { SMS, VOICE, VERIFICATION, WHATSAPP } from './services';
-declare class Sendchamp {
-    private axiosInstance;
-    SMS: SMS;
-    VOICE: VOICE;
-    VERIFICATION: VERIFICATION;
-    WHATSAPP: WHATSAPP;
-    constructor(config: SendchampConstructor);
-}
-export default Sendchamp;
+export {};
--- src/index.ts
+++ src/index.ts
@@ -38,4 +38,4 @@ class Sendchamp {
   }
 }
 
-export default Sendchamp;
+module.exports = Sendchamp;
