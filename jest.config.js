863d8a8dfd7f3952ab0b2baa262143a132041c9b
fuadop <fuadolatunji@gmail.com>
2022-02-14 10:17:20 +0100

    wip: jest config

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testMatch: ['**/__tests__/**/*.spec.[jt]s?(x)'],
+  verbose: true,
+  testEnvironment: 'node',
+  transform: {
+    'node_modules/variables/.+\\.(j|t)sx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['node_modules/(?!variables/.*)'],
+  setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
+};
